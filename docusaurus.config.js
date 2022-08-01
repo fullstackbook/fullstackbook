5668293b49c1c880f85fcd63907ecd843b61ad70
Travis Luong <travis@travisluong.com>
2022-08-01 01:13:29 -0700

    update links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/travisluong/fullstackbook',
             label: 'GitHub',
             position: 'right',
           },
@@ -115,7 +115,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/travisluong/fullstackbook',
               },
             ],
           },
