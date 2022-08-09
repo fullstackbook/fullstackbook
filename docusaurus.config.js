4f1cd0c9b713525a5b664f7000e90214aa0206a7
Travis Luong <travis@travisluong.com>
2022-08-08 20:35:06 -0700

    google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-49B2N8W371',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
